6b6b1365f3d44a009e0c98b046e2947e1d378dd5
dassimanuel000 <dassimanuel000@gmail.com>
2022-04-24 15:20:09 +0000

    fact 1.0

--- shopeein/lib/screens/home/index.dart
+++ shopeein/lib/screens/home/index.dart
@@ -1,7 +1,9 @@
-// ignore_for_file: prefer_const_constructors, avoid_print, sized_box_for_whitespace, prefer_const_literals_to_create_immutables, unnecessary_new
+// ignore_for_file: prefer_const_constructors, avoid_print, sized_box_for_whitespace, prefer_const_literals_to_create_immutables, unnecessary_new, avoid_unnecessary_containers
 
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:nb_utils/nb_utils.dart';
 import 'package:shopeein/constant/color.dart';
 import 'package:shopeein/screens/home/apphome.dart';
 import 'package:shopeein/screens/home/navBarItems.dart';
@@ -15,6 +17,14 @@ class Index extends StatefulWidget {
 }
 
 class _IndexState extends State<Index> {
+  final List<String> imgList = [
+    'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+    'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+    'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+    'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+  ];
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
   @override
@@ -51,10 +61,19 @@ class _IndexState extends State<Index> {
               )),
         ),
         Container(
-            margin: const EdgeInsets.only(bottom: 5),
-            child: Image.network(
-              "https://i.ibb.co/1RLBHyh/swiper-Home.png",
-            )),
+          height: context.width() * 0.8,
+          margin: const EdgeInsets.only(bottom: 5),
+          child: CarouselSlider(
+            options: CarouselOptions(),
+            items: imgList
+                .map((item) => Container(
+                      child: Center(
+                          child: Image.network(item,
+                              fit: BoxFit.cover, width: 1000)),
+                    ))
+                .toList(),
+          ),
+        ),
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: Container(
